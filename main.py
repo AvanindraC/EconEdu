f23aff098f63a5c7ba7cae72e31e98ee86fe1a6b
Avanindra Chakraborty <77975448+AvanindraC@users.noreply.github.com>
2021-10-09 05:43:48 +0000

    Added Log In and added some css

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0',port=8080)
